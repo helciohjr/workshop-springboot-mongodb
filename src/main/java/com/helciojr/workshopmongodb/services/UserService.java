a48be0bc8957e6982c449449c552b17dbd605b46
Helcio Junior <helcio_0011@hotmail.com>
2020-05-02 12:53:31 -0300

    Insercao de usuario com POST

--- src/main/java/com/helciojr/workshopmongodb/services/UserService.java
+++ src/main/java/com/helciojr/workshopmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.helciojr.workshopmongodb.domain.User;
+import com.helciojr.workshopmongodb.dto.UserDTO;
 import com.helciojr.workshopmongodb.repository.UserRepository;
 import com.helciojr.workshopmongodb.services.exception.ObjectNotFoundException;
 
@@ -22,6 +23,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 	
 	
