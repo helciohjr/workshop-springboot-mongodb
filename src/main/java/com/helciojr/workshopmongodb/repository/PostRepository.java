98547c5e7f3ce3dd99da77da560bfd6e9b9dab38
Helcio Junior <helcio_0011@hotmail.com>
2020-05-02 16:09:54 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/helciojr/workshopmongodb/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.helciojr.workshopmongodb.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.helciojr.workshopmongodb.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
