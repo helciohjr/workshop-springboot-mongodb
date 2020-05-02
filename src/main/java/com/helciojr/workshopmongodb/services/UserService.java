6c7865f2b933d263f6d6bffff7d2bdb69c389e09
Helcio Junior <helcio_0011@hotmail.com>
2020-05-02 12:20:18 -0300

    Obtendo um usuario por id

--- src/main/java/com/helciojr/workshopmongodb/services/UserService.java
+++ src/main/java/com/helciojr/workshopmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.helciojr.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.helciojr.workshopmongodb.domain.User;
 import com.helciojr.workshopmongodb.repository.UserRepository;
+import com.helciojr.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,10 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 }
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
 	}
